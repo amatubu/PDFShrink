82cf7578be4dcbd13f158e1c4a7b25b232393943
Naoki IIMURA <amatubu@gmail.com>
2012-09-25 23:08:14 +0900

    起動時に空のドキュメントが開くのを抑制

---
+++ PDFShrink/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  PDFShrink
+//
+//  Created by naoki iimura on 9/25/12.
+//  Copyright (c) 2012 naoki iimura. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppDelegate : NSObject
+
+@end
---
+++ PDFShrink/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  PDFShrink
+//
+//  Created by naoki iimura on 9/25/12.
+//  Copyright (c) 2012 naoki iimura. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL) applicationShouldOpenUntitledFile: (NSApplication *) application
+{
+    return NO;
+}
+
+@end
