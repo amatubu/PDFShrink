7c5bca6b744622d4ab3bdaad29941b0d8d00dcad
Naoki IIMURA <amatubu@gmail.com>
2013-01-18 21:10:29 +0900

    ドキュメントを MarkDown 形式をベースにし、配布用の RTF は pandoc を使って生成するように修正。

--- create_archive.sh
+++ create_archive.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 
+# アプリケーションをビルド
+
 xcodebuild
 
+# ドキュメントを RTF 形式に
+
+pandoc -s README.md -o ReadMe.rtf
+pandoc -s CHANGES.md -o Changes.rtf
+
+# 古い Zip ファイルを削除
+
 rm pdfshrink.zip
+
+# リリース用 Zip ファイルを生成
 
 cd build/Release
 zip -r ../../pdfshrink.zip PDFShrink.app
 cd ../..
 zip -u pdfshrink.zip ReadMe.rtf Changes.rtf LICENSE
+
+# リリース用 Zip ファイルを公開
 
 cp pdfshrink.zip ~/Dropbox/Public/
